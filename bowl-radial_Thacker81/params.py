658ca983eae58aff96f535172ba939a0479038e7
Randy LeVeque <rjl@uw.edu>
2026-07-24 18:47:51 -0700

    params.py

---
+++ bowl-radial_Thacker81/params.py
@@ -0,0 +1,14 @@
+
+# set parameters defining problem
+
+# depth at center of parabolic bowl:
+D0 = 2.
+
+# radius of B=0 contour:
+L = 3.
+
+# initial surface elevation at center of bowl:
+eta0 = 1.
+
+# gravitational force:
+grav = 9.81
